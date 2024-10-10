418845fa97252de7ff63caa12d66e8e23b02ddb9
Carlos Alberto Souza Nascimento <carlosalbertosn.10@gmail.com>
2024-10-10 18:46:41 -0300

    adicionando as combinações de cores aos cards

--- index.js
+++ index.js
@@ -7,12 +7,33 @@ createApp({
             loading: true,
             searchText: '',
             nextPage: 1,
+            typeColors: {
+                fire: '#c27e10',
+                grass: '#4CAF50',
+                water: '#00BFFF',
+                bug: '#98e880',
+                normal: '#A9A9A9',
+                poison: '#9e5cda',
+                electric: '#ffd365',
+                ground: '#9e7e52',
+                ghost: '#5626de',
+                fighting: '#ba082a',
+                psychic: '#e39fa4',
+                rock: '#897975',
+                ice: '#42bed3',
+                steel: '#999999',
+                dark: '#12124f',
+                flying: '#23f1c7',
+                fairy: '#f040f3',
+                dragon: '#3263cc',
+            }
         }
     },
     created() {
         console.log("Componente criado.");
         this.callAPI();
         window.addEventListener('scroll', this.handleScroll);
+        this.generateTypeCombinations();
     },
     beforeUnmount() {
         window.removeEventListener('scroll', this.handleScroll);
@@ -27,7 +48,7 @@ createApp({
         async callAPI() {
             console.log("Chamando API...");
             try {
-                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${(this.nextPage - 1) * 151}&limit=${151}`);
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${(this.nextPage - 1) * 20}&limit=20`);
                 if (!response.ok) throw new Error("Erro na resposta da API");
                 const data = await response.json();
                 const pokemonDetailsPromises = data.results.map(pokemon => this.fetchPokemonData(pokemon.url));
@@ -73,9 +94,29 @@ createApp({
                 return '';
             }
             if (pokemon.types.length > 1) {
-                return `${pokemon.types[0].type.name}-${pokemon.types[1].type.name}`;
+                const type1 = pokemon.types[0].type.name;
+                const type2 = pokemon.types[1].type.name;
+                return `${type1}-${type2}`;
             }
             return pokemon.types[0].type.name;
+        },
+        generateTypeCombinations() {
+            const typeKeys = Object.keys(this.typeColors);
+            typeKeys.forEach((type1) => {
+                typeKeys.forEach((type2) => {
+                    if (type1 !== type2) {
+                        const className = `${type1}-${type2}`;
+                        const style = document.createElement('style');
+                        style.type = 'text/css';
+                        style.innerHTML = `
+                            .${className} {
+                                background: linear-gradient(to right, ${this.typeColors[type1]} 50%, ${this.typeColors[type2]} 50%);
+                            }
+                        `;
+                        document.getElementsByTagName('head')[0].appendChild(style);
+                    }
+                });
+            });
         }
     }
 }).mount("#app");
